39c501a6742e2021ff003dfb44dcdc7085863ff1
McKade Sorensen <dmsorensen@alaska.edu>
2021-02-23 10:28:23 -0900

    Added deploy name

--- env.sh
+++ env.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 if (( $# != 1 )); then
-    echo "Command structure: source env.sh aws_profile_name"
+    echo "Command structure: source env.sh aws_profile_name deploy_name"
 else
     export AWS_PROFILE=$1
 
     AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id --profile "$AWS_PROFILE")
     AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key --profile "$AWS_PROFILE")
-    AWS_REGION=$(aws configure get region --profile "$AWS_PROFILE" || echo $AWS_DEFAULT_REGION)
+    AWS_REGION=$(aws configure get region --profile "$AWS_PROFILE" || echo "$AWS_DEFAULT_REGION")
+    DEPLOY_NAME=$2
 
     export AWS_ACCESS_KEY_ID
     export AWS_SECRET_ACCESS_KEY
     export AWS_REGION
+    export DEPLOY_NAME
 
     echo " environment vars:"
     echo "  AWS_PROFILE:          $AWS_PROFILE"
     echo "  AWS_REGION:           $AWS_REGION"
+    echo "  DEPLOY_NAME:          $DEPLOY_NAME"
 fi
